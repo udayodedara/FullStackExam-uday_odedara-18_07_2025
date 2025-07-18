836183d5feea7a7e6d489176879af9a91d526742
Uday Odedara <udayodibusiness@gmail.com>
2025-07-18 15:28:07 +0530

    fix: image whitelist domains

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['localhost'],
+    domains: ['localhost', 'fullstackexam-uday-odedara-18-07-2025.onrender.com'],
   },
 };
 
